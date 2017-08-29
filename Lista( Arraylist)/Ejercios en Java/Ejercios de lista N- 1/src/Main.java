0a0d2d73ca7538b6d2527f2348d72762847ee8c0
Michael Daniel Murillo López <mmurillolo1@uniminuto.edu.co>
2017-08-28 21:15:36 -0500

    Add files via upload

---
+++ Lista( Arraylist)/Ejercios en Java/Ejercios de lista N- 1/src/Main.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Michael Daniel Murillo López
+ * Estrutura de Datos
+ * ID:534830 Corporación
+ * Universitaria Minuto de Dios - UNIMINUTO 
+ * NRC:7273
+ */
+/*
+Implementar una función que recibe una
+lista de enteros L y un número entero n de forma
+que modifique la lista mediante el borrado de todos
+los elementos de la lista que tengan este valor.  
+*/
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
