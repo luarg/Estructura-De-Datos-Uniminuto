d0a92d60127461d4c312c1945a40c1b0a5bc7380
Michael Daniel Murillo López <mmurillolo1@uniminuto.edu.co>
2017-09-12 12:18:12 -0500

    Add files via upload

--- Lista( Arraylist)/Ejercicios en C/Ejercicios de Lista N-5/Main.cpp
+++ Lista( Arraylist)/Ejercicios en C/Ejercicios de Lista N-5/Main.cpp
@@ -5,21 +5,63 @@
  */
 
 /* 
- * File:   Main.cpp
- * Author: Usuario
- *
- * Created on 12 de septiembre de 2017, 11:27 AM
+ * File:   Main.c++
+ * Author: Michael Daniel Murillo López
+ * Estrutura de Datos
+ * ID:534830 
+ * Corporación Universitaria Minuto de Dios - UNIMINUTO 
+ * NRC:7273
  */
-
+/*Construir una función que sume los elementos de una lista de enteros recursivamente.
+ */
+#include <cstdio>
 #include <cstdlib>
+#include <iostream>
+/**
+Suma con una funcion recursiva
 
+**/
+ 
 using namespace std;
-
-/*
- * 
- */
-int main(int argc, char** argv) {
-
-    return 0;
+int sumar(int[],int);
+int main(int argc, char *argv[])
+{
+    int *arreglo;
+    int resultado=0;
+    int tamano=0;
+    system("color 0c");//cambio de color de pantalla
+ 
+ 
+    printf("Programa que Suma con una Funcion Recursiva\n\n\n");
+    printf("Ingrese el tamano del arreglo:  ");
+    scanf("%i",&tamano);
+ 
+    arreglo = new int[tamano];
+ 
+    for(int i=0;i<tamano;i++){
+ 
+    printf("Ingrese el elemento Numero %i :",i+1);
+    scanf("%i",&arreglo[i]);
+ 
+    }
+ 
+    resultado = sumar(arreglo,tamano);
+    printf("\nEl Resultado de la Suma es: %i",resultado);
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    return EXIT_SUCCESS;
 }
+ 
+//Esta es la funcion recursiva de suma
+int sumar(int a[],int n)
+{
+    if(n==1)
+    {
+        return a[0];
+    }else{
+        return a[n-1]+sumar(a,n-1);
+    }
+}
+
 
